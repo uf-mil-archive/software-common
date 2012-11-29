2057307efb90725a9c481e5a2efafc7b1382e654
Forrest Voight <forrest.voight@gmail.com>
2012-11-29 14:39:45 -0500

    made allowed sonar configuration values accessible

--- imagenex_852/src/imagenex_852/driver.py
+++ imagenex_852/src/imagenex_852/driver.py
@@ -4,48 +4,54 @@ class Device(object):
     def __init__(self, port_filename):
         self.port = serial.Serial(port_filename, 115200, 8, 'N', 1)
     
-    def send_switch_data_command(self,
-        range_meters=5, reverse=False, start_gain_dB=32, train_angle_degrees=0,
-        sector_width_degrees=30, step_size_degrees=3, pulse_length_usec=1,
-        data_points=500, switch_delay_msec=0, frequency_kHz=675,
-    ):
+    switch_data_command_desc = [
+        # name, default, allowed_values
+        ('range_meters', 5, [5, 10, 20, 30, 40, 50]),
+        ('reverse', False, [False, True]),
+        ('start_gain_dB', 32, range(0, 40+1)),
+        ('train_angle_degrees', 0, [x for x in xrange(-210, 210+1) if x % 3 == 0]),
+        ('sector_width_degrees', 30, [x for x in xrange(0, 360+1) if x % 3 == 0]),
+        ('step_size_degrees', 3, [0, 3, 6]),
+        ('pulse_length_usec', 1, range(1, 255+1)),
+        ('data_points', 500, [250, 500]),
+        ('switch_delay_msec', 0, [x for x in xrange(0, 255+1) if x != 253]),
+        ('frequency_kHz', 675, [675, 850]),
+    ]
+    def send_switch_data_command(self, **kwargs):
+        c = {}
+        for name, default, allowed_values in self.switch_data_command_desc:
+            # make sure options are valid
+            c[name] = kwargs.get(name, default)
+            if c[name] not in allowed_values:
+                raise ValueError('invalid value: %s = %r' % (name, c[name]))
+        
         inner_data = ''
         inner_data += chr(0x10)                     # Byte 2; Head ID; 0x10 only
-        assert range_meters in [5, 10, 20, 30, 40, 50]
-        inner_data += chr(range_meters)             # Byte 3; Range; 5 to 50 Meters; 5,10,20,30,40,50 Meters supported
+        inner_data += chr(c['range_meters'])        # Byte 3; Range; 5 to 50 Meters; 5,10,20,30,40,50 Meters supported
         inner_data += chr(0)                        # Byte 4; Reserved
-        inner_data += chr(2**6 if reverse else 0)   # Byte 5; Rev
+        inner_data += chr(2**6 if c['reverse'] else 0) # Byte 5; Rev
         inner_data += chr(0)                        # Byte 6; Reserved
         inner_data += chr(0)                        # Byte 7; Reserved
-        assert 0 <= start_gain_dB <= 40
-        inner_data += chr(start_gain_dB)            # Byte 8; Start Gain; 0 to 40dB in 1dB increments
+        inner_data += chr(c['start_gain_dB'])       # Byte 8; Start Gain; 0 to 40dB in 1dB increments
         inner_data += chr(0)                        # Byte 9; Reserved
         inner_data += chr(20)                       # Byte 10; Absorption; 20 = 0.2 dB/m 675,850 kHz
-        assert -210 <= train_angle_degrees <= 210 and train_angle_degrees % 3 == 0
-        inner_data += chr((train_angle_degrees + 210)//3) # Byte 11; Train Angle
-        assert 0 <= sector_width_degrees <= 360 and sector_width_degrees % 3 == 0
-        inner_data += chr(sector_width_degrees//3)  # Byte 12; Sector Width
-        assert step_size_degrees in [0, 3, 6]
-        inner_data += chr(step_size_degrees//3)     # Byte 13; Step Size
-        assert 1 <= pulse_length_usec <= 255
-        inner_data += chr(pulse_length_usec)        # Byte 14; Pulse Length
+        inner_data += chr((c['train_angle_degrees'] + 210)//3) # Byte 11; Train Angle
+        inner_data += chr(c['sector_width_degrees']//3) # Byte 12; Sector Width
+        inner_data += chr(c['step_size_degrees']//3) # Byte 13; Step Size
+        inner_data += chr(c['pulse_length_usec'])   # Byte 14; Pulse Length
         inner_data += chr(0)                        # Byte 15; Reserved
         inner_data += chr(0)                        # Byte 16; Reserved
         inner_data += chr(0)                        # Byte 17; Reserved
         inner_data += chr(0)                        # Byte 18; Reserved
-        assert data_points in [250, 500]
-        inner_data += chr(data_points//10)          # Byte 19; Data Points; 25 - 250 data points are returned by the head; 50 - 500 data points are returned by the head
+        inner_data += chr(c['data_points']//10)     # Byte 19; Data Points; 25 - 250 data points are returned by the head; 50 - 500 data points are returned by the head
         inner_data += chr(0)                        # Byte 20; Reserved
         inner_data += chr(0)                        # Byte 21; Reserved
         inner_data += chr(0)                        # Byte 22; Reserved
         inner_data += chr(0)                        # Byte 23; Reserved
-        assert 0 <= switch_delay_msec <= 255 and switch_delay_msec != 253
-        inner_data += chr(switch_delay_msec)        # Byte 24; Switch Delay
-        assert frequency_kHz in [675, 850]
-        inner_data += chr(1 if frequency_kHz == 850 else 0) # Byte 25; Frequency; 0 = 675 kHz; 1 = 850 kHz
+        inner_data += chr(c['switch_delay_msec'])   # Byte 24; Switch Delay
+        inner_data += chr(1 if c['frequency_kHz'] == 850 else 0) # Byte 25; Frequency; 0 = 675 kHz; 1 = 850 kHz
         
-        if '\xfd' in inner_data:
-            raise ValueError('inner_data cannot contain 0xFD byte')
+        assert '\xfd' not in inner_data, 'inner_data cannot contain 0xFD byte'
         wrapped_data = '\xfe\x44' + inner_data + '\xfd'
         self.port.write(wrapped_data)
     
