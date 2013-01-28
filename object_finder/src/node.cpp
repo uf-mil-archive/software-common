382e714bb052b93f4b44939046f8663f2aa3044a
Forrest Voight <forrest@forre.st>
2013-01-27 21:49:19 -0500

    object_finder: scanline hinting optimization

--- object_finder/src/node.cpp
+++ object_finder/src/node.cpp
@@ -108,6 +108,58 @@ bool intersect_plane_sphere(Vector3d plane_axis1, Vector3d plane_axis2,
     return true;
 }
 
+bool _accumulate_sphere_scanline(const vector<Vector3d>& sumimage, const CameraInfoConstPtr& cam_info, Vector3d sphere_pos_camera, double sphere_radius, const Matrix<double, 3, 4> &proj, uint32_t yy, Vector3d &total_color, double &count) {
+    double y = yy;
+    
+    // solution space of project((X, Y, Z)) = (x, y, z) with y fixed
+    Vector3d plane1(
+        0,
+        -((cam_info->P[7] + cam_info->P[6]) - y*(cam_info->P[11] + cam_info->P[10]))/(cam_info->P[ 5] - y*cam_info->P[9]),
+        1);
+    Vector3d plane2(
+        1,
+        (y*cam_info->P[8] - cam_info->P[4])/(cam_info->P[5] - y*cam_info->P[9]),
+        0);
+    
+    double hit1_axis1, hit1_axis2;
+    double hit2_axis1, hit2_axis2;
+    if(!intersect_plane_sphere(plane1, plane2, sphere_pos_camera, sphere_radius, hit1_axis1, hit1_axis2, hit2_axis1, hit2_axis2)) {
+        // sphere doesn't intersect scanline
+        //cout << yy << " miss" << endl;
+        return false;
+    }
+    
+    Vector3d point1 = hit1_axis1 * plane1 + hit1_axis2 * plane2;
+    Vector3d point2 = hit2_axis1 * plane1 + hit2_axis2 * plane2;
+    
+    Vector3d point1_homo = proj * point1.homogeneous();
+    if(point1_homo(2) <= 0)
+        return true; // behind camera
+    Vector2d point1_screen = point1_homo.hnormalized();
+    assert(abs(point1_screen(1) - y) < 1e-3);
+    
+    Vector3d point2_homo = proj * point2.homogeneous();
+    if(point2_homo(2) <= 0)
+        return true; // behind camera
+    Vector2d point2_screen = point2_homo.hnormalized();
+    assert(abs(point2_screen(1) - y) < 1e-3);
+    
+    double minx = min(point1_screen(0), point2_screen(0));
+    double maxx = max(point1_screen(0), point2_screen(0));
+    
+    int xstart = max(0, min((int)cam_info->width, (int)ceil(minx)));
+    int xend   = max(0, min((int)cam_info->width, (int)ceil(maxx))); // not inclusive
+    
+    total_color += (xend == 0 ? Vector3d::Zero() : sumimage[yy * cam_info->width + xend - 1]) - (xstart == 0 ? Vector3d::Zero() : sumimage[yy * cam_info->width + xstart - 1]);
+    count += xend - xstart;
+    
+    /*cout << yy << " "
+        << "(" << point1_screen(0) << " " << point1_screen(1) << ") "
+        << "(" << point2_screen(0) << " " << point2_screen(1) << ")" << endl;*/
+    
+    return true;
+}
+
 void sphere_query(const vector<Vector3d>& sumimage, const CameraInfoConstPtr& cam_info, Vector3d sphere_pos_camera, double sphere_radius, Vector3d &total_color, double &count) {
     // get the sum of the color values (along with the count) of the pixels
     // that are included within the provided sphere specified by
@@ -118,58 +170,19 @@ void sphere_query(const vector<Vector3d>& sumimage, const CameraInfoConstPtr& ca
         for(int col = 0; col < 4; col++)
             proj(row, col) = cam_info->P[4*row + col];
     
+    Vector2d center_screen = (proj * sphere_pos_camera.homogeneous()).eval().hnormalized();
+    int32_t y_center_hint = max(min(center_screen(1), (double)cam_info->height-1), 0.) + .5;
+    
     total_color = Vector3d(0, 0, 0);
     count = 0;
     
-    //cout << endl;
-    for(uint32_t yy = 0; yy < cam_info->height; yy++) {
-        double y = yy;
-        
-        // solution space of project((X, Y, Z)) = (x, y, z) with y fixed
-        Vector3d plane1(
-            0,
-            -((cam_info->P[7] + cam_info->P[6]) - y*(cam_info->P[11] + cam_info->P[10]))/(cam_info->P[ 5] - y*cam_info->P[9]),
-            1);
-        Vector3d plane2(
-            1,
-            (y*cam_info->P[8] - cam_info->P[4])/(cam_info->P[5] - y*cam_info->P[9]),
-            0);
-        
-        double hit1_axis1, hit1_axis2;
-        double hit2_axis1, hit2_axis2;
-        if(!intersect_plane_sphere(plane1, plane2, sphere_pos_camera, sphere_radius, hit1_axis1, hit1_axis2, hit2_axis1, hit2_axis2)) {
-            // sphere doesn't intersect scanline
-            //cout << yy << " miss" << endl;
-            continue;
-        }
-        
-        Vector3d point1 = hit1_axis1 * plane1 + hit1_axis2 * plane2;
-        Vector3d point2 = hit2_axis1 * plane1 + hit2_axis2 * plane2;
-        
-        Vector3d point1_homo = proj * point1.homogeneous();
-        if(point1_homo(2) <= 0)
-            continue; // behind camera
-        Vector2d point1_screen = point1_homo.hnormalized();
-        assert(abs(point1_screen(1) - y) < 1e-3);
-        
-        Vector3d point2_homo = proj * point2.homogeneous();
-        if(point2_homo(2) <= 0)
-            continue; // behind camera
-        Vector2d point2_screen = point2_homo.hnormalized();
-        assert(abs(point2_screen(1) - y) < 1e-3);
-        
-        double minx = min(point1_screen(0), point2_screen(0));
-        double maxx = max(point1_screen(0), point2_screen(0));
-        
-        int xstart = max(0, min((int)cam_info->width, (int)ceil(minx)));
-        int xend   = max(0, min((int)cam_info->width, (int)ceil(maxx))); // not inclusive
-        
-        total_color += (xend == 0 ? Vector3d::Zero() : sumimage[yy * cam_info->width + xend - 1]) - (xstart == 0 ? Vector3d::Zero() : sumimage[yy * cam_info->width + xstart - 1]);
-        count += xend - xstart;
-        
-        /*cout << yy << " "
-            << "(" << point1_screen(0) << " " << point1_screen(1) << ") "
-            << "(" << point2_screen(0) << " " << point2_screen(1) << ")" << endl;*/
+    for(int32_t yy = y_center_hint; yy >= 0; yy--) {
+        if(!_accumulate_sphere_scanline(sumimage, cam_info, sphere_pos_camera, sphere_radius, proj, yy, total_color, count))
+            break;
+    }
+    for(int32_t yy = y_center_hint + 1; yy < (int32_t)cam_info->height; yy++) {
+        if(!_accumulate_sphere_scanline(sumimage, cam_info, sphere_pos_camera, sphere_radius, proj, yy, total_color, count))
+            break;
     }
 }
 
