d5ee049c221602a29e8f779ff1c76a1c6ab4bcb6
Forrest Voight <forrest@forre.st>
2013-06-06 17:36:52 -0400

    rise_6dof: moved from euler angles to quaternion-based

--- rise_6dof/src/rise_6dof/controller.py
+++ rise_6dof/src/rise_6dof/controller.py
@@ -1,53 +1,9 @@
 from __future__ import division
 
-import math
-
 import numpy
 
-import roslib
-roslib.load_manifest('rise_6dof')
 from tf import transformations
-from geometry_msgs.msg import Vector3, Wrench
-from uf_common.orientation_helpers import xyz_array, xyzw_array
-
-
-def _jacobian(x):
-    # maps body linear+angular velocities -> global linear velocity/euler rates
-    sphi, cphi = math.sin(x[3]), math.cos(x[3])
-    stheta, ctheta, ttheta = math.sin(x[4]), math.cos(x[4]), math.tan(x[4])
-    spsi, cpsi = math.sin(x[5]), math.cos(x[5])
-    
-    J = numpy.zeros((6, 6))
-    J[0:3, 0:3] = [
-        [ ctheta * cpsi, -cphi * spsi + sphi * stheta * cpsi,  sphi * spsi + cphi * stheta * cpsi],
-        [ ctheta * spsi,  cphi * cpsi + sphi * stheta * spsi, -sphi * cpsi + cphi * stheta * spsi],
-        [-stheta       ,                sphi * ctheta       ,                cphi * ctheta       ],
-    ]
-    J[3:6, 3:6] = [
-        [1, sphi * ttheta,  cphi * ttheta],
-        [0, cphi         , -sphi         ],
-        [0, sphi / ctheta,  cphi / ctheta],
-    ]
-    return J
-
-def _jacobian_inv(x):
-    # maps global linear velocity/euler rates -> body linear+angular velocities
-    sphi, cphi = math.sin(x[3]), math.cos(x[3])
-    stheta, ctheta = math.sin(x[4]), math.cos(x[4])
-    spsi, cpsi = math.sin(x[5]), math.cos(x[5])
-    
-    J_inv = numpy.zeros((6, 6))
-    J_inv[0:3, 0:3] = [
-        [       ctheta * cpsi              ,        ctheta * spsi              ,        -stheta],
-        [sphi * stheta * cpsi - cphi * spsi, sphi * stheta * spsi + cphi * cpsi,  sphi * ctheta],
-        [cphi * stheta * cpsi + sphi * spsi, cphi * stheta * spsi - sphi * cpsi,  cphi * ctheta],
-    ]
-    J_inv[3:6, 3:6] = [
-        [1,     0,       -stheta],
-        [0,  cphi, sphi * ctheta],
-        [0, -sphi, cphi * ctheta],
-    ]
-    return J_inv
+from uf_common.orientation_helpers import quat_to_rotvec
 
 class Controller(object):
     '''
@@ -66,19 +22,30 @@ class Controller(object):
         self._rise_term = numpy.zeros(6)
         self._rise_term_int_prev = numpy.zeros(6)
     
-    def update(self, dt, desired_posetwist, current_posetwist):
-        x = numpy.concatenate([xyz_array(current_posetwist.pose.position), transformations.euler_from_quaternion(xyzw_array(current_posetwist.pose.orientation))])
-        x_dot = _jacobian(x).dot(numpy.concatenate([xyz_array(current_posetwist.twist.linear), xyz_array(current_posetwist.twist.angular)]))
+    def update(self, dt, desired, current):
+        ((desired_p, desired_o), (desired_p_dot, desired_o_dot)), ((p, o), (p_dot, o_dot)) = desired, current
         
-        xd = numpy.concatenate([xyz_array(desired_posetwist.pose.position), transformations.euler_from_quaternion(xyzw_array(desired_posetwist.pose.orientation))])
-        xd_dot = _jacobian(xd).dot(numpy.concatenate([xyz_array(desired_posetwist.twist.linear), xyz_array(desired_posetwist.twist.angular)]))
+        world_from_body = transformations.quaternion_matrix(o)[:3, :3]
+        x_dot = numpy.concatenate([
+            world_from_body.dot(p_dot),
+            world_from_body.dot(o_dot),
+        ])
         
+        world_from_desiredbody = transformations.quaternion_matrix(desired_o)[:3, :3]
+        desired_x_dot = numpy.concatenate([
+            world_from_body.dot(desired_p_dot),
+            world_from_body.dot(desired_o_dot),
+        ])
         
-        def smallest_coterminal_angle(x):
-            return (x + math.pi) % (2*math.pi) - math.pi
-        error_position_world = numpy.concatenate([xd[0:3] - x[0:3], map(smallest_coterminal_angle, xd[3:6] - x[3:6])]) # e_1 in paper
+        error_position_world = numpy.concatenate([
+            desired_p - p,
+            quat_to_rotvec(transformations.quaternion_multiply(
+                desired_o,
+                transformations.quaternion_inverse(o),
+            )),
+        ])
         
-        error_velocity_world = (xd_dot + self.config['k'] * error_position_world) - x_dot # e_2 in paper
+        error_velocity_world = (desired_x_dot + self.config['k'] * error_position_world) - x_dot
         
         if self.config['use_rise']:
             rise_term_int = self.config['ks']*self.config['alpha']*error_velocity_world + self.config['beta']*numpy.sign(error_velocity_world)
@@ -86,13 +53,12 @@ class Controller(object):
             self._rise_term += dt/2*(rise_term_int + self._rise_term_int_prev)
             self._rise_term_int_prev = rise_term_int
             
-            output = _jacobian_inv(x).dot(self.config['ks'] * error_velocity_world + self._rise_term)
+            output = self.config['ks'] * error_velocity_world + self._rise_term
         else:
             # zero rise term so it doesn't wind up over time
             self._rise_term = numpy.zeros(6)
             self._rise_term_int_prev = numpy.zeros(6)
             
-            output = _jacobian_inv(x).dot(self.config['ks'] * error_velocity_world)
+            output = self.config['ks'] * error_velocity_world
         
-        
-        return Wrench(force=Vector3(x=output[0], y=output[1], z=output[2]), torque=Vector3(x=output[3], y=output[4], z=output[5]))
+        return world_from_body.T.dot(output[0:3]), world_from_body.T.dot(output[3:6])
