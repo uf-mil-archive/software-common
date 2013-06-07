57240e1e7b05a2337ef152d1245a831c0ea5292f
Forrest Voight <forrest@forre.st>
2013-06-07 18:23:08 -0400

    c3_trajectory/rise_6dof/uf_common: added acceleration and drag feedforward terms to controller

--- rise_6dof/src/rise_6dof/controller.py
+++ rise_6dof/src/rise_6dof/controller.py
@@ -23,7 +23,7 @@ class Controller(object):
         self._rise_term_int_prev = numpy.zeros(6)
     
     def update(self, dt, desired, current):
-        ((desired_p, desired_o), (desired_p_dot, desired_o_dot)), ((p, o), (p_dot, o_dot)) = desired, current
+        ((desired_p, desired_o), (desired_p_dot, desired_o_dot), (desired_p_dotdot, desired_o_dotdot)), ((p, o), (p_dot, o_dot)) = desired, current
         
         world_from_body = transformations.quaternion_matrix(o)[:3, :3]
         x_dot = numpy.concatenate([
@@ -36,6 +36,10 @@ class Controller(object):
             world_from_body.dot(desired_p_dot),
             world_from_body.dot(desired_o_dot),
         ])
+        desired_x_dotdot = numpy.concatenate([
+            world_from_body.dot(desired_p_dotdot),
+            world_from_body.dot(desired_o_dotdot),
+        ])
         
         error_position_world = numpy.concatenate([
             desired_p - p,
@@ -47,18 +51,22 @@ class Controller(object):
         
         error_velocity_world = (desired_x_dot + self.config['k'] * error_position_world) - x_dot
         
+        pd_output = self.config['ks'] * error_velocity_world
+        
+        output = pd_output
         if self.config['use_rise']:
             rise_term_int = self.config['ks']*self.config['alpha']*error_velocity_world + self.config['beta']*numpy.sign(error_velocity_world)
             
-            self._rise_term += dt/2*(rise_term_int + self._rise_term_int_prev)
+            self._rise_term = self._rise_term + dt/2*(rise_term_int + self._rise_term_int_prev)
             self._rise_term_int_prev = rise_term_int
             
-            output = self.config['ks'] * error_velocity_world + self._rise_term
+            output = output + self._rise_term
         else:
             # zero rise term so it doesn't wind up over time
             self._rise_term = numpy.zeros(6)
             self._rise_term_int_prev = numpy.zeros(6)
-            
-            output = self.config['ks'] * error_velocity_world
+        output = output + self.config['accel_feedforward'] * desired_x_dotdot
+        output = output + self.config['vel_feedforward'] * desired_x_dot
         
-        return world_from_body.T.dot(output[0:3]), world_from_body.T.dot(output[3:6])
+        wrench_from_vec = lambda output: (world_from_body.T.dot(output[0:3]), world_from_body.T.dot(output[3:6]))
+        return wrench_from_vec(pd_output), wrench_from_vec(output)
