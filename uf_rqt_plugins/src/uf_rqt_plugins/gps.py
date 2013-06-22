c8b9cba9893374366652b2fff0a730f8a039af2e
Navid Shahrestani <nshahrestani@mil-wall-e-x.mil.ece.ufl.edu>
2013-06-22 15:58:55 -0400

    uf_rqt_plugins:gps waypoint plugin ui

--- uf_rqt_plugins/src/uf_rqt_plugins/gps.py
+++ uf_rqt_plugins/src/uf_rqt_plugins/gps.py
@@ -15,6 +15,9 @@ from python_qt_binding.QtGui import QWidget, QPushButton,QCheckBox,QListWidget,Q
 
 uipath = os.path.dirname(os.path.realpath(__file__))
 
+def pos_callback(msg):
+        global 
+
 class GPSPlugin(Plugin):
     def __init__(self, context):
         super(GPSPlugin, self).__init__(context)
@@ -22,19 +25,22 @@ class GPSPlugin(Plugin):
 
         self._widget = QWidget()
         loadUi(os.path.join(uipath, 'gps.ui'), self._widget)
-        context.add_widget(self._widget)
+        context.add_widget(self._widget)control/controller/src/pd_controller.py
 
-        self.waypoint = rospy.Publisher('/gps_waypoint',NavSatFix)
+        self.waypoint = rospy.Publisher('/gps_latlong_waypoint',NavSatFix)
+        rospy.Subscriber('/gps_pos',PointStamped,pos_callback)
 
-        self._widget.findChild(QPushButton, 'publish_waypoint').clicked.connect(self._on_publish_clicked)
-        self._widget.findChild(QPushButton, 'record_waypoint').clicked.connect(self._on_record_clicked)
-       
+        self._widget.findChild(QPushButton, 'record_entered_waypoint').clicked.connect(self._on_record_entered_clicked)
+        self._widget.findChild(QPushButton, 'record_current_waypoint').clicked.connect(self._on_record_current_clicked)
+        self._widget.findChild(QPushButton, 'publish_waypoint_list').clicked.connect(self._on_pub_list_clicked)
+        self._widget.findChild(QPushButton, 'delete_').clicked.connect(self._on_delete_clicked)
                 
-
-    def _on_publish_clicked(self):
-        self.lat = self._widget.findChild(QLineEdit, 'lat_in').displayText()
-        self.long = self._widget.findChild(QLineEdit, 'long_in').displayText()
-        self.alt = self._widget.findChild(QLineEdit, 'alt_in').displayText()
+    def _on_pub_list_clicked(self):  
+        self.rec_waypoint = self._widget.findChild(QListWidget, 'waypoint_list').currentItem().text()
+        self.list = self.rec_waypoint.split(',')
+        self.lat = self.list[1]
+        self.long = self.list[2]
+        self.alt = self.list[3]
         self.waypoint.publish(NavSatFix(
 	        header=Header(
 		        frame_id='/world',
@@ -43,9 +49,28 @@ class GPSPlugin(Plugin):
                 longitude = float(self.long),
                 altitude = float(self.alt),      
 	        ))
+    
+    def _on_record_current_clicked(self):
+        self.name = self._widget.findChild(QLineEdit, 'waypoint_name').displayText()
+        self._widget.findChild(QLineEdit, 'waypoint_name').clear()
+        self._widget.findChild(QListWidget, 'waypoint_list').addItem(str(self.name+','+'1'+','+'2'+','+'3'))
 
-    def _on_record_clicked(self):
-        pass  
+    def _on_record_entered_clicked(self):
+        self.lat = self._widget.findChild(QLineEdit, 'lat_in').displayText()
+        self.long = self._widget.findChild(QLineEdit, 'long_in').displayText()
+        self.alt = self._widget.findChild(QLineEdit, 'alt_in').displayText()
+        self.name = self._widget.findChild(QLineEdit, 'waypoint_name').displayText()  
+        self._widget.findChild(QLineEdit, 'lat_in').clear()
+        self._widget.findChild(QLineEdit, 'long_in').clear()
+        self._widget.findChild(QLineEdit, 'alt_in').clear()
+        self._widget.findChild(QLineEdit, 'waypoint_name').clear()
+        
+        self._widget.findChild(QListWidget, 'waypoint_list').addItem(str(self.name+','+self.lat+','+self.long+','+self.alt))
+
+    def _on_delete_clicked(self):
+       for SelectedItem in self._widget.findChild(QListWidget, 'waypoint_list').selectedItems():
+               self._widget.findChild(QListWidget, 'waypoint_list').takeItem(self._widget.findChild(QListWidget, 'waypoint_list').row(SelectedItem))
+        
 
     
     
