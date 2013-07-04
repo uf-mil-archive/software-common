8f112bb0795afc69f851455186ed62e6a3e42291
PropaGator <propagator@propagator.(none)>
2013-07-04 18:06:39 -0400

    Initial version of Engineering Plant gui with kill buttons

--- uf_rqt_plugins/src/uf_rqt_plugins/engineering_plant.py
+++ uf_rqt_plugins/src/uf_rqt_plugins/engineering_plant.py
@@ -13,9 +13,26 @@ from qt_gui.plugin import Plugin
 from rawgps_common.gps import ecef_from_latlongheight,enu_from_ecef
 from python_qt_binding import loadUi
 from python_qt_binding.QtCore import SIGNAL,QTimer,QSettings
-from python_qt_binding.QtGui import QWidget, QPushButton,QCheckBox,QListWidget,QLineEdit
+from python_qt_binding.QtGui import QWidget, QPushButton,QCheckBox,QListWidget,QLineEdit,QProgressBar
+from power_router.msg import prstatus
+from power_router.srv import SetKill
+
 
 uipath = os.path.dirname(os.path.realpath(__file__))
+
+global computer_current, motor_current, temperature, battery
+computer_current = []
+motor_current = []
+temperature = []
+battery = []
+
+def data_update(msg):
+	global computer_current, motor_current, temperature, battery
+	computer_current = msg.computercurrent
+	motor_current = msg.motorcurrent
+	temperature = msg.temperature
+	battery = msg.battery
+
 
 
 class Engineering_Plant(Plugin):
@@ -28,9 +45,11 @@ class Engineering_Plant(Plugin):
         context.add_widget(self._widget)
 
         self._widget.findChild(QPushButton, 'Kill').clicked.connect(self.Kill)
-        self._widget.findChild(QPushButton, 'Un-Kill').clicked.connect(self.Un_kill)
-
-        
+        self._widget.findChild(QPushButton, 'UnKill').clicked.connect(self.Un_kill)
+	
+	rospy.Subscriber('/power_router/status',prstatus,data_update)	
+        self.killservice = rospy.ServiceProxy('/power_router/setkill', SetKill)
+	
         self._update_timer = QTimer(self._widget)
         self._update_timer.timeout.connect(self._on_update)
         self._update_timer.start(1000)
@@ -38,7 +57,21 @@ class Engineering_Plant(Plugin):
       
 
     def _on_update(self):
-		pass      
+	global computer_current, motor_current, temperature, battery
+	compratio = int(float(computer_current/5000.0)*100)
+	self._widget.findChild(QProgressBar, 'ComputerCurrent').setValue(compratio)	
+	motorratio = int(float(motor_current/10000.0)*100)
+	self._widget.findChild(QProgressBar, 'MotorCurrent').setValue(motorratio)
+	tempratio = int(float(temperature/150)*100)
+	self._widget.findChild(QProgressBar, 'Temperature').setValue(tempratio)
+	battratio = int(float(battery/100)*100)
+	self._widget.findChild(QProgressBar, 'Battery').setValue(battratio)
+	    
+    def Kill(self):
+		 self.killservice(True)
+    def Un_kill(self):
+	         self.killservice(False)
+ 
 
                 
   
