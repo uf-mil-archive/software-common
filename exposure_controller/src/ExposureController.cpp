247c304fcb1af3e1161ab4824f51e8eb28592a2e
gbdash <gautam.dash@gmail.com>
2013-07-05 12:03:19 -0400

    Added dynparams to change exposure. Need to plot exposure vs heuristic curves before proceeding

--- exposure_controller/src/ExposureController.cpp
+++ exposure_controller/src/ExposureController.cpp
@@ -4,6 +4,8 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
 #include "exposure_controller/metered_exposure.h"
 
 namespace enc = sensor_msgs::image_encodings;
@@ -16,6 +18,11 @@ class ExposureController
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   ros::Publisher pub_;
+  std::stringstream outputFile_; 
+  std::stringstream commandVar_; 
+  boost::filesystem::ofstream fileStream_; 
+  float exp_tm_ms_;
+  int sysret_;
   
 public:
   ExposureController()
@@ -23,63 +30,38 @@ public:
   {
     image_sub_ = it_.subscribe("test_image", 1, &ExposureController::imageCb, this);
     pub_ = nh_.advertise<exposure_controller::metered_exposure>("test_metered_exposure",600);
+    outputFile_ << "exp_cntrl.log";
+    exp_tm_ms_ = 10.0;
+    sysret_ = system("rosrun dynamic_reconfigure dynparam set /ueye_manager zoom 4");
+    sysret_ += system("rosrun dynamic_reconfigure dynparam set /ueye_manager auto_exposure false");
+    commandVar_ << "rosrun dynamic_reconfigure dynparam set /ueye_manager exposure_time " << exp_tm_ms_;
+    sysret_ += system(commandVar_.str().c_str());
+    sysret_ += system("rosrun dynamic_reconfigure dynparam set /ueye_manager hardware_gamma false");
+    sysret_ += system("rosrun dynamic_reconfigure dynparam set /ueye_manager gain_boost false");
+    sysret_ += system("rosrun dynamic_reconfigure dynparam set /ueye_manager auto_gain false");
+    sysret_ += system("rosrun dynamic_reconfigure dynparam set /ueye_manager frame_rate 10");
+ 
+    if(sysret_==0)
+        ROS_INFO("Camera successfully initialized!");
+    else
+        ROS_INFO("Error during camera initialization. Camera may be unresponsive!");
+ 
+    try{
+        fileStream_.open(outputFile_.str().c_str());
+    }
+    catch(const boost::filesystem::filesystem_error& ex){
+        ROS_DEBUG("Caught File System Error: %s", ex.what());
+        exit(1);
+    }
   }
 
   ~ExposureController()
   {
-  
+     fileStream_.close(); 
   }
   
-  float weighted2e(cv::Mat &img)
-  {
-    int rows = img.rows;
-    int cols = img.cols;
-    float weights[5];
-    float weight = 0;
-    float areas[5];
-    float area = 0;
-    
-    int i;
-    for(i=0;i<5;i++){
-        areas[i] = (i+1)*rows*cols*5/100;
-        weights[i] = 2.0/exp(i);
-    }
-    
-    int r;
-    int c;
-    float sum = 0.0;
-    int count = 0;
-    float cen_r = (rows+1)/2.0;
-    float cen_c = (cols+1)/2.0;
-    for(r=0;r<rows;r++)
-        for(c=0;c<cols;c++){
-            weight = 0;
-            area = pi*(pow((r-cen_r),2.0) + pow((c-cen_c),2.0));
-            if( area < areas[4] ){
-                weight = weights[4];
-                if( area < areas[3] ){
-                    weight = weights[3];
-                    if( area < areas[2] ){
-                        weight = weights[2];
-                        if( area < areas[1] ){
-                            weight = weights[1];
-                            if( area < areas[0] ){
-                                weight = weights[0];
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if(weight > 0.001){
-                ++count;
-                sum += weight*(img.at<cv::Vec3b>(r,c)[0]);
-            }
-        }
-    return sum/count;
-  } 
-
-  float weighted2l(cv::Mat &img)
+ 
+  float weighteds(cv::Mat &img)
   {
     int rows = img.rows;
     int cols = img.cols;
@@ -126,65 +108,44 @@ public:
         }
     return sum/count;
   } 
-  
-  float weighted22(cv::Mat &img)
+    
+  float averageLab(cv::Mat &img)
   {
     int rows = img.rows;
     int cols = img.cols;
-    float weights[5];
-    float weight = 0;
-    float areas[5];
-    float area = 0;
-    
-    int i;
-    for(i=0;i<5;i++){
-        areas[i] = (i+1)*rows*cols*5/100;
-        weights[i] = 2/pow(2,i);
-    }
 
     int r;
     int c;
-    float sum = 0.0;
+    double sum = 0.0;
     int count = 0;
-    float cen_r = (rows+1)/2.0;
-    float cen_c = (cols+1)/2.0;
     for(r=0;r<rows;r++)
         for(c=0;c<cols;c++){
-            weight = 0;
-            area = pi*(pow((r-cen_r),2.0) + pow((c-cen_c),2.0));
-            if( area < areas[4] ){
-                weight = weights[4];
-                if( area < areas[3] ){
-                    weight = weights[3];
-                    if( area < areas[2] ){
-                        weight = weights[2];
-                        if( area < areas[1] ){
-                            weight = weights[1];
-                            if( area < areas[0] ){
-                                weight = weights[0];
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if(weight > 0.001){
-                ++count;
-                sum += weight*(img.at<cv::Vec3b>(r,c)[0]);
-            }
+            ++count;
+            sum += img.at<cv::Vec3b>(r,c)[0];
         }
-    return sum/count;
-  } 
+    return sum*100.0/(255.0*count);
+  }
+  
+  void logdata(exposure_controller::metered_exposure expmsg)
+  {
+    try{
+        fileStream_ << expmsg.exp_tms << ", ";
+        fileStream_ << expmsg.lab_l << ", ";
+        fileStream_ << expmsg.exp2l << std::endl;
+    }
+    catch(const boost::filesystem::filesystem_error& ex){
+        ROS_DEBUG("Caught File System Error: %s", ex.what());
+        exit(1);
+    } 
+  }
   
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
     cv_bridge::CvImageConstPtr cvimage;
-    try
-    {
+    try{
       cvimage = cv_bridge::toCvShare(msg, enc::BGR8);
     }
-    catch (cv_bridge::Exception& e)
-    {
+    catch (cv_bridge::Exception& e){
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
@@ -199,13 +160,18 @@ public:
     exposure_controller::metered_exposure expmsg;
     
     expmsg.header = cvimage->header;
-    expmsg.exp2e = weighted2e(imgLab);
-    expmsg.exp2l = weighted2l(imgLab);
-    expmsg.exp22 = weighted22(imgLab);  
-    expmsg.exp_tms = img.at<cv::Vec3b>(0,0)[0];    
-    expmsg.lab_l = imgLab.at<cv::Vec3b>(0,0)[0]*100.0/255.0;
+    expmsg.exp2l = weighteds(imgLab);
+    expmsg.exp_tms = exp_tm_ms_;    
+    expmsg.lab_l = averageLab(imgLab);
+    
+    exp_tm_ms_ = exp_tm_ms_ * 1.1;
+    if(exp_tm_ms_ > 40) exp_tm_ms_ = 1;
+    commandVar_.str("");
+    commandVar_ << "rosrun dynamic_reconfigure dynparam set /ueye_manager exposure_time " << exp_tm_ms_;
+    sysret_ += system(commandVar_.str().c_str());
     
     pub_.publish(expmsg);  
+    logdata(expmsg);
   }
 };
 
