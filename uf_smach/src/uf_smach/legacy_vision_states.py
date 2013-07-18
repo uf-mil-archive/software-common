08f026b1e8afa233828624218a788f80225900c7
Matt Thompson <matthewbot@gmail.com>
2013-07-18 00:25:19 -0400

    uf_smach: Added gain argument to legacy vision approaches.

--- uf_smach/src/uf_smach/legacy_vision_states.py
+++ uf_smach/src/uf_smach/legacy_vision_states.py
@@ -113,6 +113,11 @@ class BaseManeuverObjectState(smach.State):
             self._shared['moveto'].send_goal(goal)
 
 class CenterObjectState(BaseManeuverObjectState):
+    def __init__(self, *args, **kwargs):
+        self._gain = kwargs.pop('gain', 2)
+        BaseManeuverObjectState.__init__(self, *args, **kwargs)
+
+
     def _get_goal(self, result, current, (tf_p, tf_q)):
         vec = numpy.array(map(float, result['center'])); vec /= numpy.linalg.norm(vec)
         vec_world = transformations.quaternion_matrix(tf_q)[:3, :3].dot(vec)
@@ -125,13 +130,14 @@ class CenterObjectState(BaseManeuverObjectState):
         # get rid of component going along camera axis
         vec_world2 = vec_world - camera_axis*camera_axis.dot(vec_world)
         
-        vel_world = 2*vec_world2
+        vel_world = self._gain*vec_world2
         if numpy.linalg.norm(vel_world) > .2:
             vel_world = .2 * vel_world/numpy.linalg.norm(vel_world)
         return current.as_MoveToGoal(linear=current._rot.T.dot(vel_world))
 
 class CenterApproachObjectState(BaseManeuverObjectState):
     def __init__(self, *args, **kwargs):
+        self._gain = kwargs.pop('gain', 1.5)
         self._desired_scale = kwargs.pop('desired_scale')
         BaseManeuverObjectState.__init__(self, *args, **kwargs)
 
@@ -151,7 +157,7 @@ class CenterApproachObjectState(BaseManeuverObjectState):
         # get rid of component going along camera axis
         vec_world2 = vec_world - camera_axis*camera_axis.dot(vec_world)
         
-        vel_world = 1.5*vec_world2 # TODO make customizable
+        vel_world = self._gain*vec_world2 # TODO make customizable
         if numpy.linalg.norm(vel_world) > .2:
             vel_world = .2 * vel_world/numpy.linalg.norm(vel_world)
         else:
