3cbc94691eb80a8536339188220d8ff58e33e25f
Forrest Voight <forrest@forre.st>
2013-07-25 09:41:39 -0700

    uf_smach: removed annoying prints

--- uf_smach/src/uf_smach/legacy_vision_states.py
+++ uf_smach/src/uf_smach/legacy_vision_states.py
@@ -88,9 +88,7 @@ class BaseManeuverObjectState(smach.State):
         return 'succeeded' if self._done else 'failed'
 
     def _feedback_cb(self, feedback):
-        print 'a'
         with self._cond:
-            print 'b'
             good_results = map(json.loads, feedback.targetreses[0].object_results)
             #print good_results
             if len(good_results) == 0:
@@ -101,9 +99,7 @@ class BaseManeuverObjectState(smach.State):
                 return
             else:
                 self._fail_ctr = 0
-            print 'c'
             current = PoseEditor.from_PoseTwistStamped_topic('/trajectory')
-            print 1
             try:
                 world_from_result_tf = self._shared['tf_listener'].lookupTransform(current.frame_id, feedback.header.frame_id, rospy.Time(0))
             except:
