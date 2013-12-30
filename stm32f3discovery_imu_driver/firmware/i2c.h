958295485ef560aaf62ae2cbaa411acbae26efbc
Forrest Voight <forrest@forre.st>
2013-12-30 16:47:49 -0500

    removed packages moved to other repositories

--- stm32f3discovery_imu_driver/firmware/i2c.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BRMYSZLJUPSYELNW_
-#define _BRMYSZLJUPSYELNW_
-
-#include <stm32f3discovery_imu_driver/protocol.h>
-
-void i2c_setup(void);
-void i2c_read_imu(stm32f3discovery_imu_driver::GetIMUDataResponse &resp);
-
-#endif
