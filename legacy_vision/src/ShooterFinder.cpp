018699e618f5dc2cc9062bbe7621bd4d2d746eb9
Eugene <elroberts56@gmail.com>
2014-05-31 08:20:17 -0400

    legacy_vision: "Tweaks with shooter finder to work with alien brunch task"

--- legacy_vision/src/ShooterFinder.cpp
+++ legacy_vision/src/ShooterFinder.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <iostream>
 
+#include "Blob.h"
 #include "Contours.h"
 #include "Normalizer.h"
 #include "Thresholder.h"
@@ -14,73 +15,38 @@ using namespace cv;
 using namespace std;
 
 IFinder::FinderResult ShooterFinder::find(const subjugator::ImageSource::Image &img) {
-	Thresholder thresholder(img.image);
+	Mat blurred; GaussianBlur(img.image, blurred, Size(0, 0), 3);
 
-	// call to thresholder here
-	Mat dbg = thresholder.simpleHSV(config.get<uchar>(objectPath[0] + "_hue"),
-					config.get<uchar>(objectPath[0] + "_hue_range"),
-					config.get<uchar>(objectPath[0] + "_sat_C"));
+        Mat normalized = Normalizer::normRGB(blurred);
 
-	erode(dbg,dbg,cv::Mat::ones(5,5,CV_8UC1));
-	dilate(dbg,dbg,cv::Mat::ones(5,5,CV_8UC1));
+        // call to thresholder here
+        Mat thresholded = Thresholder(normalized).simpleRGB(Vec3b(0, 255, 0), Vec3b(106, 106, 43));
+        erode(thresholded, thresholded, cv::Mat::ones(13,13,CV_8UC1)); // -6
+        dilate(thresholded, thresholded, cv::Mat::ones(15,15,CV_8UC1)); // +7
+        erode(thresholded, thresholded, cv::Mat::ones(3,3,CV_8UC1)); // -1
 
-	// call to specific member function here
-	Contours contours(dbg, 50, 7000000,1500000);
+        Blob blob(thresholded, 300, 1e10, 1e10, false, true);
+        for(unsigned int i = 0; i < blob.data.size(); )
+                if(blob.data[i].circularity < .8)
+                        blob.data.erase(blob.data.begin()+i);
+                else
+                        i++;
 
-	// Draw result
-	Mat res = img.image.clone();
-	contours.drawResult(res, CV_RGB(255, 255, 255));
 
-	vector<property_tree::ptree> resultVector;
-	if(objectPath[1] == "box") {
-		// Prepare results
-		if(contours.boxes.size() && contours.shapes.size()) {
-			property_tree::ptree fResult;
-			fResult.put_child("center", Point_to_ptree(contours.boxes[0].centroid, img));
-			fResult.put("scale", contours.boxes[0].area);
-			fResult.put("angle", contours.boxes[0].orientationError);
-			resultVector.push_back(fResult);
-		}
-	} else if(objectPath[1] == "small") {
-		Contours::InnerContour bestShape;
-		bool foundSomething = false;
-		BOOST_FOREACH(const Contours::InnerContour &shape, contours.shapes)
-			if(shape.circularity > 0.5 && shape.area < shape.outer_area*2/3 && (!foundSomething || shape.area > bestShape.area)) {
-				foundSomething = true;
-				bestShape = shape;
-			}
-		if(foundSomething) {
-			Contours::InnerContour bestShape2;
-			{
-				bool foundSomething2 = false;
-				BOOST_FOREACH(const Contours::InnerContour &shape, contours.shapes)
-					if(shape.circularity > 0.5 && shape.area < shape.outer_area*2/3 && (!foundSomething2 || shape.area > bestShape2.area) && shape.area < bestShape.area) {
-						foundSomething2 = true;
-						bestShape2 = shape;
-					}
-				if(!foundSomething2)
-					bestShape2 = bestShape;
-			}
-			if(bestShape2.area < 0.1*bestShape.area) // if second largest is an order of magnitude smaller
-				bestShape2 = bestShape; // use largest
+        Mat res = img.image.clone();
+        //if(blob.data.size() > 2)
+        //        blob.data.resize(2);
+        blob.drawResult(res, CV_RGB(0, 255, 0));
 
-			circle(res, bestShape2.centroid, 10, CV_RGB(255, 255, 0), 2);
+        // Prepare results
+        vector<property_tree::ptree> resultVector;
+        BOOST_FOREACH(const Blob::BlobData &item, blob.data) {
+                property_tree::ptree fResult;
+                fResult.put_child("center", Point_to_ptree(item.rect_center, img));
+                fResult.put_child("direction", Direction_to_ptree(item.rect_center, item.direction, img));
+                fResult.put("direction_symmetry", 2);
+                resultVector.push_back(fResult);
+        }
 
-			property_tree::ptree fResult;
-			fResult.put_child("center", Point_to_ptree(bestShape2.centroid, img));
-			//fResult.put("angle", contours.boxes[0].orientationError);
-			fResult.put("scale", bestShape2.area);
-			resultVector.push_back(fResult);
-		}
-	} else { assert(objectPath[1] == "large");
-		if(contours.shapes.size()) {
-			Contours::InnerContour shape = contours.findLargestShape();
-			property_tree::ptree fResult;
-			fResult.put_child("center", Point_to_ptree(shape.centroid, img));
-			//fResult.put("angle", contours.boxes[0].orientationError);
-			fResult.put("scale", shape.area);
-			resultVector.push_back(fResult);
-		}
-	}
-	return FinderResult(resultVector, res, dbg);
+        return FinderResult(resultVector, res, thresholded);
 }
