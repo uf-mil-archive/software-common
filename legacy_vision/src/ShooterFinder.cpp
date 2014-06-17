bec60d1d391286bc4c0e34c4ee83a579b4efaace
Chan Yang <chanyang1993@gmail.com>
2014-06-17 03:56:06 -0400

    legacy_vision: changed ShooterFinder to use black thresholder

--- legacy_vision/src/ShooterFinder.cpp
+++ legacy_vision/src/ShooterFinder.cpp
@@ -15,27 +15,20 @@ using namespace cv;
 using namespace std;
 
 IFinder::FinderResult ShooterFinder::find(const subjugator::ImageSource::Image &img) {
-	Mat blurred; GaussianBlur(img.image, blurred, Size(0, 0), 2);
+        Mat blurred; GaussianBlur(img.image, blurred, Size(0, 0), 2);
 
-        Mat normalized = Normalizer::normRGB(blurred);
+        Thresholder thresholder(blurred);       
+        Mat dbg = thresholder.black();
 
-        // call to thresholder here
-        Mat thresholded = Thresholder(normalized).simpleRGB(Vec3b(85, 85, 85), Vec3b(106, 106, 43));
-        //erode(thresholded, thresholded, cv::Mat::ones(13,13,CV_8UC1)); // -6
-        //dilate(thresholded, thresholded, cv::Mat::ones(15,15,CV_8UC1)); // +7
-        erode(thresholded, thresholded, cv::Mat::ones(5,5,CV_8UC1)); // -1
-
-        Blob blob(thresholded, 300, 1e10, 1e10, false, true);
+        Blob blob(dbg, 300, 1e10, 1e10, false, true);
         for(unsigned int i = 0; i < blob.data.size(); )
-                if(blob.data[i].circularity < .6)
+                if(blob.data[i].circularity < .8)
                         blob.data.erase(blob.data.begin()+i);
                 else
                         i++;
 
+        Mat res = blurred.clone();
 
-        Mat res = normalized.clone();
-        //if(blob.data.size() > 2)
-        //        blob.data.resize(2);
         blob.drawResult(res, CV_RGB(0, 255, 0));
 
         // Prepare results
@@ -48,5 +41,5 @@ IFinder::FinderResult ShooterFinder::find(const subjugator::ImageSource::Image &
                 resultVector.push_back(fResult);
         }
 
-        return FinderResult(resultVector, res, thresholded);
+        return FinderResult(resultVector, res, dbg);
 }
